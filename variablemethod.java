d54b88d49f08acba501d084c91fe81a0e8c97209
pranathi1221 <pranathineo@gmail.com>
2022-06-14 22:54:13 +0530

    message

---
+++ variablemethod.java
@@ -0,0 +1,12 @@
+class A
+{
+    public static void main(String args[])
+	{
+	    String message = sayHalo("Pranathi");
+		System.out.println(message);
+	}
+	public static String sayHalo(String user)
+	{
+	return "Welcome to Java master " + user;
+	}
+}
